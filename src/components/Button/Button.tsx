9b1ffc477ca30eecb2f8a13bc59c90af80eddd32
Ofir Gur <ofir.gur@browzwear.com>
2022-11-20 17:42:26 +0200

    styled-components

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,22 @@
 import React from "react";
+import styled from "styled-components";
 import "./Button.scss";
 
 export interface ButtonProps {
   label: string;
 }
 
+const StyledButton = styled.button`
+  color: palevioletred;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return <StyledButton>{props.label}</StyledButton>;
 };
 
 export default Button;
